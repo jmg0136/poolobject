90e754d503396a2aec812b5eb139381afb6759a0
jmg0136 <jmg0136@alu.ubu.es>
2021-03-03 19:48:23 +0100

    Prueba testGetInstance Singleton.
    Añadidos librería jacocoant.jar actualizada y el código de los imports
    más el del procedimiento testGetInstance implementados.

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -9,17 +9,25 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ubu.gii.dass.c01.DuplicatedInstanceException;
+import ubu.gii.dass.c01.NotFreeInstanceException;
+import ubu.gii.dass.c01.Reusable;
+import ubu.gii.dass.c01.ReusablePool;
+
 /**
- * @author alumno
+ * @author jmg0136
  *
  */
 public class ReusablePoolTest {
+	
+	private ReusablePool pool;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		pool = ReusablePool.getInstance();
 	}
 
 	/**
@@ -27,6 +35,7 @@ public class ReusablePoolTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		pool = null;
 	}
 
 	/**
@@ -34,7 +43,11 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testGetInstance() {
-		fail("Not yet implemented");
+		ReusablePool pool = ReusablePool.getInstance();
+		// Se comprueba que no es nulo
+		assertNotNull(pool);
+		// Se comprueba que el objeto devuelto es una instancia de ReusablePool
+		assertTrue(pool instanceof ReusablePool);
 	}
 
 	/**
